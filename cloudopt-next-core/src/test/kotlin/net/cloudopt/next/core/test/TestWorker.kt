21b46f7e3fa53a0c66fcbb04b641601788e81931
Andy <dr.qin@protonmail.com>
2022-03-23 16:02:41 +0800

    feat: Add test cases of gather method

--- cloudopt-next-core/src/test/kotlin/net/cloudopt/next/core/test/TestWorker.kt
+++ cloudopt-next-core/src/test/kotlin/net/cloudopt/next/core/test/TestWorker.kt
@@ -2,13 +2,14 @@ package net.cloudopt.next.core.test
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import net.cloudopt.next.core.Worker
-import net.cloudopt.next.core.Worker.async
 import net.cloudopt.next.core.Worker.await
 import net.cloudopt.next.core.Worker.cancelTimer
+import net.cloudopt.next.core.Worker.gather
 import net.cloudopt.next.core.Worker.global
 import net.cloudopt.next.core.Worker.setTimer
 import net.cloudopt.next.core.Worker.then
@@ -78,23 +79,45 @@ class TestWorker {
     }
 
     @Test
-    fun testAsync() {
-        val text = async {
-            return@async "success"
-        }
-        assert(text == "success")
-    }
-
-    @Test
     fun testAsyncFunction() {
         val text = asyncFunction()
         assert(text == "success")
     }
 
-    private fun asyncFunction(): String = async {
-        return@async await {
+    private fun asyncFunction(): String = runBlocking {
+        return@runBlocking await {
             return@await "success"
         }
+    }
+
+    suspend fun taskA(): Int {
+        delay(5000)
+        return 1
+    }
+
+    suspend fun taskB(): Int {
+        delay(5000)
+        return 2
+    }
+
+    @Test
+    fun testGather(): Unit = runBlocking {
+        assert(gather<Int>(
+            {
+                await {
+                    return@await 1
+                }
+            },
+            {
+                await {
+                    return@await 2
+                }
+            }
+        ).sum() == 3)
+        val startTime = System.currentTimeMillis()
+        assert(gather({ taskA() }, { taskB() }).sum() == 3)
+        val endTime = System.currentTimeMillis()
+        assert((endTime - startTime) in 5000..6000)
     }
 
     @Test
